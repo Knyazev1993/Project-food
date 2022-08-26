eba22331a5be6235c39acd33b7f3942fb1717d75
Knyazev1993 <Kniazevyuriya@yandex.ru>
2022-08-26 20:37:52 +0400

    Modal btns

--- js/1 script.js
+++ js/1 script.js
@@ -97,19 +97,37 @@ window.addEventListener('DOMContentLoaded', () => {
     //Modal window;
 
 
-    const modalTrigger = document.querySelector('[data-modal]'),
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
         modal = document.querySelector('.modal'),
         modalCloseBtn = document.querySelector('[data-close]');
 
-
-    modalTrigger.addEventListener('click', () => {
-        modal.classList.add('show');
-        modal.classList.remove('hide');
-
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
 
     });
-    modalCloseBtn.addEventListener('click', () => {
+
+    function closeModal() {
         modal.classList.add('hide');
         modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+
+
+    document.addEventListener('keydown', (event) => {
+        if (event.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
     });
 });
